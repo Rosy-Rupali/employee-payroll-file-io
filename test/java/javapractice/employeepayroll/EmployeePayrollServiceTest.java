53e9b44fffd1bda6ac0dc33f8e0f3e6d7a73e709
Rosy-Rupali <itrosyrupali4037@gmail.com>
2021-07-04 14:42:53 +0530

    added: printing employee payroll data and count of entries

--- test/java/javapractice/employeepayroll/EmployeePayrollServiceTest.java
+++ test/java/javapractice/employeepayroll/EmployeePayrollServiceTest.java
@@ -10,16 +10,15 @@ import service.EmployeePayrollService;
 public class EmployeePayrollServiceTest {
 	@Test
 	public void givenThreeEmployees_WhenWrittenToFile_ShouldMatchEmployeeEntries() {
-	EmployeePayrollData[] arrayOfEmployees = {
-			new EmployeePayrollData(1, "Rosy Rupali", 400000.00),
-			new EmployeePayrollData(2, "Rohit Kumar", 200000.00),
-			new EmployeePayrollData(3, "Anupama Singh", 300000.00)
-	};
-	EmployeePayrollService employeePayrollService = new EmployeePayrollService(Arrays.asList(arrayOfEmployees));
-	employeePayrollService.writingData(EmployeePayrollService.IOService.FILE_IO);
-	
-	long entries = employeePayrollService.countEntries(EmployeePayrollService.IOService.FILE_IO);
-	Assert.assertEquals(3, entries);
+		EmployeePayrollData[] arrayOfEmployees = { new EmployeePayrollData(1, "Rosy Rupali", 400000.00),
+				new EmployeePayrollData(2, "Rohit Kumar", 200000.00),
+				new EmployeePayrollData(3, "Anupama Singh", 300000.00) };
+		EmployeePayrollService employeePayrollService = new EmployeePayrollService(Arrays.asList(arrayOfEmployees));
+		employeePayrollService.writingData(EmployeePayrollService.IOService.FILE_IO);
+		employeePayrollService.printData(EmployeePayrollService.IOService.FILE_IO);
+		long entries = employeePayrollService.countEntries(EmployeePayrollService.IOService.FILE_IO);
+		System.out.println("Total number of entries are: "+entries);
+		Assert.assertEquals(3, entries);
 	}
 
 }
