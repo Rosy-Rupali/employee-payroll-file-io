fb743e6be556bec2ee62d55182cd82162924f767
Rosy-Rupali <itrosyrupali4037@gmail.com>
2021-07-04 14:05:26 +0530

    added: storing employee payroll data and testing total entries

---
+++ test/java/javapractice/employeepayroll/EmployeePayrollServiceTest.java
@@ -0,0 +1,25 @@
+package javapractice.employeepayroll;
+
+import java.util.Arrays;
+
+import org.junit.Assert;
+import org.junit.Test;
+import model.EmployeePayrollData;
+import service.EmployeePayrollService;
+
+public class EmployeePayrollServiceTest {
+	@Test
+	public void givenThreeEmployees_WhenWrittenToFile_ShouldMatchEmployeeEntries() {
+	EmployeePayrollData[] arrayOfEmployees = {
+			new EmployeePayrollData(1, "Rosy Rupali", 400000.00),
+			new EmployeePayrollData(2, "Rohit Kumar", 200000.00),
+			new EmployeePayrollData(3, "Anupama Singh", 300000.00)
+	};
+	EmployeePayrollService employeePayrollService = new EmployeePayrollService(Arrays.asList(arrayOfEmployees));
+	employeePayrollService.writingData(EmployeePayrollService.IOService.FILE_IO);
+	
+	long entries = employeePayrollService.countEntries(EmployeePayrollService.IOService.FILE_IO);
+	Assert.assertEquals(3, entries);
+	}
+
+}
